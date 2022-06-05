16c6e53c712261de1f3f86767c5468a66c05be86
Malgorzata Janczura <malgorzata.janczura@ista.de>
2022-06-05 18:51:55 +0200

    change main

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,12 @@ provider "azurerm" {
 }
 
 terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.50.0"
+    }
+  }
   backend "azurerm" {
     storage_account_name = ""
     container_name       = ""
